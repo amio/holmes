b21c2030a6673a28f1f5ce57a01eb4adeef813d1
Amio <amio.cn@gmail.com>
2017-12-07 10:07:03 +0800

    Manually inline svg icon

--- components/hero-icon.js
+++ components/hero-icon.js
@@ -1,25 +1,16 @@
-import Amcon from '../static/Amcon.svg'
-
-const ICON_SIZE = '128px'
-
-export default (props) => {
-  return <div>
-    <Amcon />
-    <style jsx>{`
-      div {
-        width: 12vh;
-        height: 12vh;
-        border-radius: ${ICON_SIZE};
-        background-color: #111;
-        border: 2px solid #111;
-      }
-      div :global(svg) {
-        width: 12vh;
-        height: 12vh;
-      }
-      div :global(path) {
-        fill: #FFF;
-      }
-    `}</style>
-  </div>
+export default props => {
+  return (
+    <div>
+      <svg width='1024' height='1024' xmlns='http://www.w3.org/2000/svg' style={{ background: '#000' }}>
+        <path d='M512 896l-75-128h150l-75 128zm280-192H232l280-560 280 560z' fill='#FFF' fill-rule='evenodd' />
+      </svg>
+      <style jsx>{`
+        svg {
+          border-radius: 2000px;
+          border: 2px solid #111;
+          transform: scale(0.12);
+        }
+      `}</style>
+    </div>
+  )
 }
